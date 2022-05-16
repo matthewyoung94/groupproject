dddd82a7f7d858e50e599ad0e060b96a26588bbc
Matt <99823652+paellapapi@users.noreply.github.com>
2022-05-16 16:33:51 +0100

    Update index.js

--- index.js
+++ index.js
@@ -70,11 +70,11 @@ var i;
 for (i = 0; i < coll.length; i++) {
   coll[i].addEventListener("click", function() {
     this.classList.toggle("active");
-    var content = this.nextElementSibling;
-    if (content.style.display === "block") {
-      content.style.display = "none";
+    var card = this.nextElementSibling;
+    if (card.style.display === "block") {
+      card.style.display = "none";
     } else {
-      content.style.display = "block";
+      card.style.display = "block";
     }
   });
 }
