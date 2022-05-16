75e28d2701ac9a82f9d09221cc9ce4d4aaaa1c37
Matt <99823652+paellapapi@users.noreply.github.com>
2022-05-16 14:52:23 +0100

    Update index.js

--- index.js
+++ index.js
@@ -63,3 +63,18 @@ $(window).scroll( throttle(highlightNavigation,100) );
 
 // if you don't want to throttle the function use this instead:
 // $(window).scroll( highlightNavigation );
+
+var coll = document.getElementsByClassName("card");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.display === "block") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "block";
+    }
+  });
+}
