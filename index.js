17831f60bd376b7fd5fbd97c10429b291105adba
Matt <99823652+paellapapi@users.noreply.github.com>
2022-05-17 14:41:31 +0100

    Update index.js

--- index.js
+++ index.js
@@ -116,11 +116,11 @@ var i;
 for (i = 0; i < coll.length; i++) {
   coll[i].addEventListener("click", function() {
     this.classList.toggle("active");
-    var content = this.nextElementSibling;
-    if (content.style.maxHeight){
-      content.style.maxHeight = null;
+    var collapsecontent = this.nextElementSibling;
+    if (collapsecontent.style.maxHeight){
+      collapsecontent.style.maxHeight = null;
     } else {
-      content.style.maxHeight = content.scrollHeight + "px";
+      collapsecontent.style.maxHeight = collapsecontent.scrollHeight + "px";
     } 
   });
 }
